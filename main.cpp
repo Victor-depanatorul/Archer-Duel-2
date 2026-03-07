fe8722853373cbcff2458269549820242e727036
Victor-depanatorul <@Ce1323798006>
2026-03-07 11:22:50 +0200

    Am adaugat o parte din clase

--- main.cpp
+++ main.cpp
@@ -3,36 +3,49 @@
 #include <chrono>
 #include <thread>
 #include <raylib-cpp.hpp>
+#include <utility>
 namespace miscare {
     const char MoveKeys[5] = "WASD";
     raylib::Vector2 ChangePos[4] = {{0, -10}, {-10, 0}, {0, 10}, {10, 0}};
 }
-class Caracter {
+enum tipSageti {
+    Normala,
+    Otravitoare,
+    Aimbot,
+    Healing,
+    Giganta
+};
+class Entitate {
+protected:
     raylib::Vector2 pos;
-    const char* SpritePath;
+    raylib::Rectangle rect;
+    raylib::Texture2D sprite;
 public:
-    Caracter() : pos(0, 0), SpritePath("textures/PrimavaraFrumoasaV2.png") {}
-    Caracter(raylib::Vector2 pos) : pos(pos), SpritePath("textures/PrimavaraFrumoasaV2.png") {}
-    Caracter(raylib::Vector2 pos, std::string filepath): pos(pos), SpritePath(filepath.c_str()) {}
-    Caracter(raylib::Vector2 pos, const char* filepath): pos(pos), SpritePath(filepath) {}
-    Caracter (const Caracter& other) {
-        if (this!=&other) {
-            pos = other.pos;
-            SpritePath = other.SpritePath;
-        }
-    }
-    Caracter& operator=(const Caracter& other) {
-        if (this!=&other) {
-            pos = other.pos;
-            SpritePath = other.SpritePath;
-        }
-        return *this;
-    }
-    ~Caracter() = default;
+    Entitate() = delete;
+    explicit Entitate(const raylib::Rectangle &rect) : rect(rect) {}
+    Entitate(const raylib::Rectangle &rect,const raylib::Vector2 &pos) : pos(pos), rect(rect) {}
+    Entitate(const raylib::Rectangle &rect, raylib::Texture2D &otherSprite)
+        : rect(rect), sprite(std::move(otherSprite)) {}
+    Entitate(const raylib::Vector2 &rect, const raylib::Vector2 pos, raylib::Texture2D &otherSprite) :
+    rect(rect), pos(pos), sprite(std::move(otherSprite)) {}
+    ~Entitate() = default;
+    raylib::Vector2 get_pos() const {return pos;}
+};
+class Sageata : Entitate {
+    tipSageti tip;
+    double viteza;
+};
+class Arc {
+    raylib::Vector2 pos;
+    static const int CapacitateSageti=1000;
+    std::array<Sageata, CapacitateSageti> capacitateSageti;
+};
+class Caracter  : Entitate {
+    int hp=100;
+public:
     raylib::Vector2 get_pos() const {
         return pos;
     }
-    raylib::Texture get_TextureAtPath() {return LoadTexture(SpritePath);}
     void Move() {
         for (int i = 0; i < 4; i++) {
             if (raylib::Keyboard::IsKeyDown(miscare::MoveKeys[i])) {
@@ -45,16 +58,17 @@ public:
 
 int main() {
     raylib::Window window(800, 600, "test");
-    Caracter p;
-    raylib::Texture textura=p.get_TextureAtPath();
-    window.SetTargetFPS(60);
-    while (!window.ShouldClose()) {
-        window.BeginDrawing();
-        textura.Draw(p.get_pos());
-        ClearBackground(RAYWHITE);
-        window.EndDrawing();
-        p.Move();
-    }
+    Entitate e(raylib::Rectangle(10, 10));
+    std::cout<<e.get_pos().x << ' '  << e.get_pos().y << std::endl;
+    // raylib::Texture textura=p.get_TextureAtPath();
+    // window.SetTargetFPS(60);
+    // while (!window.ShouldClose()) {
+    //     window.BeginDrawing();
+    //     textura.Draw(p.get_pos());
+    //     ClearBackground(RAYWHITE);
+    //     window.EndDrawing();
+    //     p.Move();
+    // }
 
     return 0;
 }

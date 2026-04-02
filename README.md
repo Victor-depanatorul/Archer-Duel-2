716abce1190b45d8622412cd1f874f66179ef10f
Victor-depanatorul <@Ce1323798006>
2026-04-02 15:16:32 +0300

    Adaugare demo Demo-ul testeaza interactiunea dintre obicete, pentru o viitoare implementare in cadrul jocului.

--- README.md
+++ README.md
@@ -15,15 +15,15 @@ Archer Duel (numele actual, posibile schimbari in viitor) este inspirat dupa Bat
 - [X] pentru o aceeași (singură) clasă: constructor de copiere, `operator=` de copiere, destructor
 <!-- - [ ] pentru o altă clasă: constructor de mutare, `operator=` de mutare, destructor -->
 <!-- - [ ] pentru o altă clasă: toate cele 5 funcții membru speciale -->
-- [ ] `operator<<` pentru **toate** clasele pentru afișare (`std::ostream`) folosind compunere de apeluri cu `operator<<`
-- [ ] cât mai multe `const` (unde este cazul) și funcții `private`
-- [ ] implementarea a minim 3 funcții membru publice pentru funcționalități netriviale specifice temei alese, dintre care cel puțin 1-2 funcții mai complexe
+- [X] `operator<<` pentru **toate** clasele pentru afișare (`std::ostream`) folosind compunere de apeluri cu `operator<<`
+- [X] cât mai multe `const` (unde este cazul) și funcții `private`
+- [X] implementarea a minim 3 funcții membru publice pentru funcționalități netriviale specifice temei alese, dintre care cel puțin 1-2 funcții mai complexe
   - nu doar citiri/afișări sau adăugat/șters elemente într-un/dintr-un vector
-- [ ] scenariu de utilizare **cu sens** a claselor definite:
+- [X] scenariu de utilizare **cu sens** a claselor definite:
   - crearea de obiecte și apelarea tuturor funcțiilor membru publice în main
   - vor fi adăugate în fișierul `tastatura.txt` DOAR exemple de date de intrare de la tastatură (dacă există); dacă aveți nevoie de date din fișiere, creați alte fișiere separat
 - [ ] minim 52-60% din codul propriu să fie C++, `.gitattributes` configurat corect
-- [ ] tag de `git`: de exemplu `v0.1`
+- [X] tag de `git`: de exemplu `v0.1`
 - [ ] serviciu de integrare continuă (CI) cu **toate bifele**; exemplu: GitHub Actions
 - [ ] code review #1 2 proiecte
 
--- main.cpp
+++ main.cpp
@@ -31,42 +31,77 @@ enum tipSageti : unsigned char {
     Aimbot,
     Healing,
     Giganta,
-    NrTipuri
+    NrTipuri,
+    Invalid
 };
 class Sageata {
     tipSageti tip;
-    static constexpr std::array<float, tipSageti::NrTipuri> damage = {5, 2.5, 5, 0, 10};
-    raylib::Rectangle rect;
+    static constexpr std::array<float, tipSageti::NrTipuri> damage{5, 2.5, 5, -10, 10};
+    raylib::Vector2 pos;
+
 public:
+
     explicit Sageata(tipSageti tip=Normala, float posX=-1, float posY=-1) :
-    tip(tip), rect(posX, posY, 5, 1) {}
+    tip(tip), pos(posX, posY) {}
     Sageata(const Sageata& AltaSageata) = default;
     Sageata& operator=(const Sageata& AltaSageata) {
         if (this != &AltaSageata) {
             tip = AltaSageata.tip;
-            rect = AltaSageata.rect;
+            pos = AltaSageata.pos;
         }
         return *this;
     }
 
+    ~Sageata() {
+        tip = Invalid;
+        pos.x=pos.y=-1;
+    }
     [[nodiscard]] tipSageti get_tip() const {return tip;}
 
-    [[nodiscard]] raylib::Rectangle get_rect() const{return rect;}
+    [[nodiscard]] raylib::Vector2 get_pos() const {return pos;}
+
+    [[nodiscard]] static std::array<float, tipSageti::NrTipuri> get_damage_array() {return damage;}
+
 
     friend std::ostream& operator<< (std::ostream& os, const Sageata& s) {
-        os << "Pozitia:(" << s.rect.x << ", " << s.rect.y << ")" << '\n';
-        os << "Marimea HitBox-ului:" << s.rect.width << ", " << s.rect.height << '\n';
-        os << "Tipul sagetii:" << s.tip << std::endl;
+        os << "Pozitia: (" << s.pos.x << ", " << s.pos.y << ")" << '\n';
+        os << "Tipul sagetii: ";
+        switch (s.tip) {
+            case Normala:
+                os << "Normala";
+                break;
+            case Otravitoare:
+                os << "Otravitoare";
+                break;
+            case Aimbot:
+                os << "Aimbot";
+                break;
+            case Healing:
+                os << "Healing";
+                break;
+            case Giganta:
+                os << "Giganta";
+                break;
+            default:
+                os << "Tip necunoscut!";
+                break;
+        }
+        os << std::endl;
         return os;
     }
+    void MutaSageata(float dx, float dy) {
+        pos.x=dx, pos.y=dy;
+    }
 };
-
 class Arc {
     unsigned long long CapacitateArc;
     std::vector<Sageata> Sageti;
+    std::vector<Sageata> SagetiTrase;
     public:
     explicit Arc(unsigned long long CapacitateArc=20, tipSageti tip=Normala) :
-    CapacitateArc(CapacitateArc), Sageti(CapacitateArc, Sageata(tip)) {}
+    CapacitateArc(CapacitateArc), Sageti(CapacitateArc, Sageata(tip)) {
+        SagetiTrase.reserve(CapacitateArc);
+    }
     explicit Arc(const std::vector<Sageata>& Sageti){
         CapacitateArc=Sageti.size();
         this->Sageti=Sageti;
@@ -76,8 +111,31 @@ class Arc {
         if (this != &Arc) {
             Sageti = Arc.Sageti;
             CapacitateArc = Arc.CapacitateArc;
+            SagetiTrase=Arc.SagetiTrase;
         }
         return *this;
+    }
+
+    [[nodiscard]] unsigned long long get_capacitate_arc() const {return CapacitateArc;}
+
+    [[nodiscard]] std::vector<Sageata> get_sageti() const {return Sageti;};
+
+    [[nodiscard]] std::vector<Sageata> get_sageti_trase() {return SagetiTrase;}
+
+    void TrageLa(raylib::Vector2 pozitie) {
+        Sageata s = Sageti.back();
+        s.MutaSageata(pozitie.x, pozitie.y);
+        SagetiTrase.emplace_back(s);
+        Sageti.pop_back();
+    }
+
+    void MutaSageataTrasa(float dx, float dy, size_t i) {
+        SagetiTrase[i].MutaSageata(SagetiTrase[i].get_pos().x+dx, SagetiTrase[i].get_pos().y+dy);
+    }
+    void StergeSageataTrasa(size_t i) {
+        // SagetiTrase[SagetiTrase.size()-1].MutaSageata(-1, -1);
+        if (i<SagetiTrase.size())
+        SagetiTrase.erase(SagetiTrase.begin() + static_cast<long long>(i));
     }
     friend std::ostream& operator<< (std::ostream& os, const Arc& a) {
         os << "Capacitatea arcului:" << a.CapacitateArc << '\n';
@@ -116,45 +174,80 @@ class Arc {
     }
 };
 
+
 class Caracter {
-    int hp;
+    float hp;
     raylib::Rectangle rect;
     const char* PathTextura;
     raylib::Texture2D textura;
     Arc arc;
+    float scale;
 public:
-    explicit Caracter(const char* PathTextura="../assets/textures/pacman3.png", int hp=100,
-        float posX=0, float posY=0) : hp(hp),PathTextura(PathTextura), textura(PathTextura), arc() {
+    explicit Caracter(float scale=1, float posX=0, float posY=0,
+        const char* PathTextura="../assets/textures/pacman3.png", float hp=100):
+    hp(hp), PathTextura(PathTextura), textura(PathTextura), arc(), scale(scale) {
         rect=raylib::Rectangle(posX, posY,
-            static_cast<float>(textura.GetWidth()), static_cast<float>(textura.GetHeight()));
+            static_cast<float>(textura.GetWidth())*scale, static_cast<float>(textura.GetHeight())*scale);
     }
-    explicit Caracter(const Arc& arc, const char* PathTextura="../assets/textures/pacman3.png", int hp=100,
-        float posX=0, float posY=0) : hp(hp),PathTextura(PathTextura), textura(PathTextura), arc(arc) {
+    explicit Caracter(const Arc& arc, float scale=1, float posX=0, float posY=0,
+        const char* PathTextura="../assets/textures/pacman3.png", float hp=100
+        ) :hp(hp),PathTextura(PathTextura), textura(PathTextura), arc(arc), scale(scale) {
         rect=raylib::Rectangle(posX, posY,
-            static_cast<float>(textura.GetWidth()), static_cast<float>(textura.GetHeight()));
+            static_cast<float>(textura.GetWidth())*scale, static_cast<float>(textura.GetHeight())*scale);
     }
     Caracter(const Caracter &other) : hp(other.hp), rect(other.rect), PathTextura(other.PathTextura)
-    , textura(other.PathTextura){}
+    , textura(other.PathTextura), arc(other.arc), scale(other.scale){}
     Caracter& operator=(const Caracter &other) {
         if (this != &other) {
             hp=other.hp;
             rect=other.rect;
             PathTextura=other.PathTextura;
             textura.Load(other.textura.GetData());
+            arc=other.arc;
+            scale=other.scale;
         }
         return *this;
     }
-    [[nodiscard]] Arc get_arc() const {return arc;}
+    [[nodiscard]] Arc& get_arc() {return arc;}
     void MutaCaracter() {
         for (int i=0; i<4; ++i)
             if (raylib::Keyboard::IsKeyDown(miscare::MoveKeys[i]))
-                rect.x+=miscare::ChangePos[i].x, rect.y+=miscare::ChangePos[i].y;
+                rect.SetPosition(rect.GetPosition()+=miscare::ChangePos[i]);
     }
-    void DeseneazaCaracter(float rotation=0, float scale=0.1f,
-        raylib::Color tint={255,255,255,255})
-    const {
-        textura.Draw({rect.x, rect.y}, rotation, scale, tint);
+
+    [[nodiscard]] float get_hp() const {
+        return hp;
     }
+
+    [[nodiscard]] raylib::Rectangle get_rect() const {
+        return rect;
+    }
+
+    void DeseneazaCaracter(float rotation=0) const{
+        textura.Draw(rect.GetPosition(), rotation, scale);
+    }
+    void DeseneazaHitbox (float rotation=0) const {
+        rect.Draw({0, 0}, rotation, {255, 0, 0, 100});
+    }
+    void VerColiziune (const Caracter& other) const {
+        if (rect.CheckCollision(other.get_rect())) {
+            DeseneazaHitbox();
+            other.DeseneazaHitbox();
+        }
+    }
+    friend std::ostream& operator<< (std::ostream& os, const Caracter& c) {
+        if (c.textura.IsValid()) {
+            os << "Textura incarcata cu succes din " << c.PathTextura << ".\n";
+        }
+        else {
+            os << "Ceva este gresit cu textura..." << '\n';
+            return os;
+        }
+        os << "Hp: " << c.hp << '\n' << "Pozitie: " << '(' << c.rect.x << ", "  << c.rect.y << ")\n"
+        << "Hitbox: " << '(' << c.rect.width << ", " << c.rect.height << ")" << std::endl;
+        return os;
+    }
+    friend class GameDemo;
 };
 class Bloc {
     raylib::Rectangle rect;
@@ -168,20 +261,71 @@ public:
         }
         return *this;
     }
+    friend std::ostream& operator<< (std::ostream& os, const Bloc& b) {
+        return os << "Pozitie: " << '(' << b.rect.x << ", " << b.rect.y << ")\n" <<
+            "Marimi: " << '(' << b.rect.width << ", " << b.rect.height << ")" << std::endl;
+    }
+};
+
+class GameDemo {
+    const int windowWidth = 800;
+    const int windowHeight = 450;
+    raylib::Window window;
+    Arc arc{std::vector<Sageata>(50, Sageata(Giganta))};
+    Caracter player{0.1f, 0.0f, static_cast<float>(windowHeight)/2};
+    Caracter inamic{0.1f, static_cast<float>(windowWidth)-player.rect.width,
+        static_cast<float>(windowHeight)/2};
+public:
+    GameDemo() : window(windowWidth, windowHeight) {std::cout << player << std::endl;}
+
+    static bool Nimerit(const Sageata& s, Caracter& c) {
+        if (c.rect.CheckCollision(raylib::Vector2{s.get_pos().x+20, s.get_pos().y})) {
+            c.hp-=Sageata::get_damage_array()[s.get_tip()];
+            return true;
+        }
+        return false;
+    }
+    void run() {
+        Arc arc_inamic(inamic.arc);
+        window.SetTargetFPS(60);
+        while (!window.ShouldClose()) {
+            float PlayerHp=player.get_hp(), InamicHp=inamic.get_hp();
+                window.BeginDrawing();
+            if (PlayerHp>0 && InamicHp>0) {
+                window.ClearBackground(RAYWHITE);
+                player.DeseneazaCaracter();
+                inamic.DeseneazaCaracter();
+                std::vector<Sageata> trase=arc.get_sageti_trase();
+                if (raylib::Keyboard::IsKeyPressed('C')) {
+                    arc.TrageLa(player.get_rect().GetPosition());
+                }
+                for (size_t i=0; i<trase.size(); ++i) {
+                    bool nimerit = Nimerit(trase[i], inamic);
+                    float fstartX = trase[i].get_pos().GetX();
+                    float fstartY = trase[i].get_pos().GetY();
+                    int startX=static_cast<int>(fstartX);
+                    int startY=static_cast<int>(fstartY);
+                    arc.MutaSageataTrasa(20, 0, i);
+                    DrawRectangle(startX, startY+100, 20, 20, BLUE);
+                    if (nimerit) {
+                        arc.StergeSageataTrasa(i);
+                        trase.erase(trase.begin()+static_cast<long long>(i));
+                        std::cout << inamic << std::endl;
+                    }
+                }
+            }
+            else {
+                ClearBackground(RAYWHITE);
+                const std::string mesaj="GAME OVER";
+                DrawText(mesaj.c_str(), windowWidth/2, windowHeight/2, 20, BLACK);
+            }
+                window.EndDrawing();
+        }
+    }
 };
 
 int main() {
-    raylib::Window window(800, 600, "test");
-    window.SetTargetFPS(60);
-    Caracter c;
-    std::cout << c.get_arc();
-    while (!window.ShouldClose()) {
-        window.BeginDrawing();
-        ClearBackground(BLACK);
-        c.DeseneazaCaracter(90, 0.1f);
-        c.MutaCaracter();
-        window.EndDrawing();
-    }
-
+    GameDemo game;
+    game.run();
     return 0;
 }
